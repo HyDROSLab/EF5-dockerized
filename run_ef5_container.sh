93b775e5c8a4c6acc33c72696ee22aff8c93e24a
Jorge A. Duarte <babetoduarte@gmail.com>
2023-05-05 11:16:21 -0500

    Modified run scripts to mount /data as RW, since states will be written to /data/states/ folder.

--- run_ef5_container.sh
+++ run_ef5_container.sh
@@ -4,5 +4,5 @@ function pause(){
    read -p "$*"
 }
 
-docker run -it -v $(pwd)/data:/data:ro -v $(pwd)/conf:/conf:ro -v $(pwd)/results:/results:rw -u `id -u $USER`:`id -g $USER` ef5-container
+docker run -it -v $(pwd)/data:/data:rw -v $(pwd)/conf:/conf:ro -v $(pwd)/results:/results:rw -u `id -u $USER`:`id -g $USER` ef5-container
 pause 'Press any key to continue...'
